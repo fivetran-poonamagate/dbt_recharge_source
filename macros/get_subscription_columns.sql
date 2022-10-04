9be9c5f3afeac8b722925bd234260c0b8fa2f846
Poonam Agate <poonam.agate@fivetran.com>
2022-10-04 15:47:40 +0100

    Fixing typo

--- macros/get_subscription_columns.sql
+++ macros/get_subscription_columns.sql
@@ -23,7 +23,7 @@
     {"name": "charge_interval_frequency", "datatype": dbt_utils.type_int()},
     {"name": "order_day_of_week", "datatype": dbt_utils.type_int()},
     {"name": "order_day_of_month", "datatype": dbt_utils.type_int()},
-    {"name": "expire_after_specific_numer_of_charges", "datatype": dbt_utils.type_int()},
+    {"name": "expire_after_specific_number_of_charges", "datatype": dbt_utils.type_int()},
     {"name": "number_charges_until_expiration", "datatype": dbt_utils.type_int()},
     {"name": "updated_at", "datatype": dbt_utils.type_timestamp()},
     {"name": "_fivetran_deleted", "datatype": dbt_utils.type_timestamp()},
