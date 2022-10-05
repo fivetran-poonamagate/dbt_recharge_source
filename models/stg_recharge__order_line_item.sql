9bf6db43573f0f491d07b13f7e2a8403ae0cd858
Poonam Agate <poonam.agate@fivetran.com>
2022-10-05 13:55:59 +0100

    Adding macro files and updating table definition cont

--- models/stg_recharge__order_line_item.sql
+++ models/stg_recharge__order_line_item.sql
@@ -1,14 +1,27 @@
 
-with order_line_item as (
+with base as (
     select
         *
     from
-        {{ source('recharge','order_line_item') }}
+        {{ ref('stg_recharge__order_line_item_tmp') }}
 )
 
+, fields as (
+    select
+        {{
+            fivetran_utils.fill_staging_columns(
+                source_columns = adapter.get_columns_in_relation(ref('stg_recharge__order_line_item_tmp')),
+                staging_columns = get_order_line_item_columns()
+            )
+        }}
+    from 
+        base
+)
+
+, final as (
 select
     order_id
-    , index as order_line_item_index
+    , index
     , subscription_id
     , shopify_product_id
     , product_title as product_title_1
@@ -18,12 +31,18 @@ select
     , quantity
     , grams
     , price
-    , property_charge_interval_unit_type
-    , property_charge_interval_frequency
-    , property_shipping_interval_unit_type
-    , property_shipping_interval_frequency
+    , property_charge_interval_unit_type as charge_interval_unit_type
+    , property_charge_interval_frequency as charge_interval_frequency
+    , property_shipping_interval_unit_type as shipping_interval_unit_type
+    , property_shipping_interval_frequency as shipping_interval_frequency
     , property_title
-    , property_type
     , property_price
+    , cast(_fivetran_synced as {{ dbt_utils.type_timestamp() }}) as _fivetran_synced
 from
-    order_line_item
+    fields
+)
+
+select
+    *
+from
+    final
